c9f31fb172d2b9c8699256d6cc90451847d46a64
Katharine McClenaghan <kemcclen@gmail.com>
2023-07-11 20:41:21 -0400

    date formatting added

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const {
     getThoughts,
     getSingleThought,
-   // createThought,
+   createThought,
     updateThought,
     deleteThought,
     addReaction,
@@ -10,7 +10,7 @@ const {
 } = require('../../controllers/thoughtController');
 
 // /api/thoughts
-// router.route('/').get(getThoughts).post(createThought);
+router.route('/').get(getThoughts).post(createThought);
 
 // /api/thoughts/:thoughtId
 router.route('/:thoughtId').get(getSingleThought).delete(deleteThought);
